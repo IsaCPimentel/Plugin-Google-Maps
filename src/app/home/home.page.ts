3cd59261af6135a64d817d2298126bc493739740
DESKTOP-FBDB4O7\Isadora <isadora.pimentel@voacolibri.com.br>
2025-11-12 20:13:53 -0300

    implementação inicial de marcadores fixos no mapa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { GoogleMap } from '@capacitor/google-maps';
+import { GoogleMap, Marker } from '@capacitor/google-maps';
+import { ModalController } from '@ionic/angular';
+import { ModalPage } from '../modal/modal.page';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -12,7 +14,10 @@ export class HomePage {
   @ViewChild('map')mapRef!: ElementRef;
   map!: GoogleMap;
 
-  constructor() {}
+  constructor(
+    private modalCtrl: ModalController,
+
+  ) {}
 
   ionViewDidEnter() {
     this.createMap();
@@ -25,11 +30,50 @@ export class HomePage {
       element: this.mapRef.nativeElement,
       config: {
         center: {
-          lat: 33.6,
-          lng: -117.9,
+          // coordenadas de Belo Horizonte
+          lat: -20.0,
+          lng: -43.75,
         },
-        zoom: 8,
+        zoom: 7
       },
+    });
+    this.addMarkers();
+  }
+
+  async addMarkers() {
+    const markers: Marker[] = [
+      {
+        coordinate: {
+          lat: -19.88,
+          lng: -43.95
+        },
+        title: 'Colibri',
+        snippet: 'Escritório de Belo Horizonte'
+      },
+      {
+        coordinate: {
+          lat: -23.33,
+          lng: -46.38
+        },
+        title: 'Liberdade',
+        snippet: 'Bairro da Liberdade em São Paulo'
+      },
+    ];
+    
+    const result =  await this.map.addMarkers(markers);
+    console.log(result);
+
+    this.map.setOnMarkerClickListener(async(marker) => {
+      
+      const modal = await this.modalCtrl.create({
+        component: ModalPage,
+        componentProps: {
+          marker,
+        },
+        breakpoints: [0, 0.3],
+        initialBreakpoint: 0.3,
+      });
+      await modal.present();
     });
   }
 }
